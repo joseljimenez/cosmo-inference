f62657ab6a3a98b5dab5ed8b079bef580aad020f
jose <joseluisjimenez026@gmail.com>
2026-02-24 15:35:33 -0600

    Proyecto Completo a Subir en GitHub

--- data_download.sh
+++ data_download.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd Data
+cd data
 
 # DESI data from the Cobaya repository
 git clone https://github.com/CobayaSampler/bao_data.git
@@ -14,10 +14,10 @@ git clone https://github.com/rubind/union3_release
 
 # Pantheon+ data from a repository by the Pantheon+SH0ES collaboration. 
 # The download also includes SH0ES 2022 data. https://arxiv.org/pdf/2112.04510
-git clone https://github.com/PantheonPlusSH0ES/DataRelease.git
-mv DataRelease PantheonPlusSH0ES
+# git clone https://github.com/PantheonPlusSH0ES/DataRelease.git
+# mv DataRelease PantheonPlusSH0ES
 
 
 
 # DES-SNe data from the DES-science repository
-git clone https://github.com/des-science/DES-SN5YR
+# git clone https://github.com/des-science/DES-SN5YR
